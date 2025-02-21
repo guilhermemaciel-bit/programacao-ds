53ae6e75d60dc4d4882a01f2aa3736222922bbc2
guilhermemaciel-bit <maciel.bueno.guilherme@escola.pr.gov.br>
2025-02-21 09:33:37 -0300

    Create Teste_alura.py

---
+++ Teste_alura.py
@@ -0,0 +1,30 @@
+# Impressão configurada com separador " "
+subst = "Python" 
+verbo = "é" 
+adjetivo = "fantástico" print(subst, verbo, adjetivo, sep="_", end="!\n") 
+
+# Soma impossível de inteiro e string 
+idade1 = 10
+idade2 = "20" 
+print(idade1 + idade2)
+
+# Soma possível após conversão de string para inteiro
+idade1 = 10 
+idade2 = int("20") 
+print(idade1 + idade2)
+
+# Concatenação de strings
+nome = "Nico" 
+sobrenome = "Steppat" 
+print(nome + sobrenome) 
+
+Siga os passos (veja a ordem na imagem):  
+- abra o seu repositório programacao-no-ds (criado na atividade 20/02)
+- clicar em Add file
+- selecionar Create new file
+- colar o nome do novo arquivo testes_alura.py
+- copiar/colar os 4 códigos acima na área Edit 
+- clicar no botão Commit changes
+- na janela que abrirá, clicar em Commit changes 
+
+Copie o link do seu repositório atualizado, que está na barra de endereços. O arquivo novo deve aparecer na lista. 
